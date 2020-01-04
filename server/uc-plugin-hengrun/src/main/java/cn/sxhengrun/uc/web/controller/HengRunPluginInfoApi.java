5654941a286e00a8007cff3626b4b07d022fbee5
cFrost <cFrost@eulerproject.io>
2020-01-04 18:29:03 +0800

    UC plugin

---
+++ server/uc-plugin-hengrun/src/main/java/cn/sxhengrun/uc/web/controller/HengRunPluginInfoApi.java
@@ -0,0 +1,17 @@
+package cn.sxhengrun.uc.web.controller;
+
+import org.eulerframework.web.core.annotation.ApiEndpoint;
+import org.eulerframework.web.core.base.controller.ApiSupportWebController;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@ApiEndpoint
+@RequestMapping("plugin/hengrun/info")
+public class HengRunPluginInfoApi extends ApiSupportWebController {
+
+    @GetMapping
+    public String helloWorld() {
+        return "Hengrun plugin enabled";
+    }
+
+}
